31d3425f9feb5b85bb4ca674eada6343ee210ce3
ARJUN-TUDU <64701428+ARJUN-TUDU@users.noreply.github.com>
2024-07-18 17:43:58 +0530

    daw

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,11 @@ import { Container } from 'react-bootstrap'
 const Home = () => {
   return (
  
-        <div className='' style={{height:"100px",width:"100%",width:"100%",backgroundColor:"red"}}>DddAW</div>
+        <div className='' style={{height:"100px",width:"100%",width:"100%",backgroundColor:"red"}}>
+            
+            cristiano ronaldo
+            
+        </div>
     
   )
 }
