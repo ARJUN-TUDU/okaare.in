783caf94ef7f72d462d133aadb730f99a43210d5
ARJUN-TUDU <64701428+ARJUN-TUDU@users.noreply.github.com>
2024-07-26 20:13:59 +0530

    dwadddddddd:

--- src/components/Events.js
+++ src/components/Events.js
@@ -11,7 +11,7 @@ const Events = ({profile_id}) => {
 
     const addVisiting = async(event_id,profile_id) => {
 
-
+        
        
         try{
             const res = await axios.post("http://localhost:5000/findEvent",{profile_id:profile_id,event_id:event_id})
@@ -55,19 +55,20 @@ const Events = ({profile_id}) => {
                  {
                     evenList.map((x)=>{
                         return  <Col lg = {6} sm = {12} xs = {12} style = {{gap:"5px"}} >
-                        <Card  style = {{minHeight:"180px"}} > 
+                        <Card  style = {{minHeight:"180px",padding:""}} > 
 
                             <Card.Body style = {{display:"",flexDirection:"",justifyContent:""}}>
-                                
-                                 <div><p className='desc_font'>{x.name}</p>
-                                <hr></hr>
-                              <p className='header_Font' style = {{fontWeight:"bold"}}  >   {x.desc}</p></div>
+                                <Card.Img src = {require("../pic2.jpg")}></Card.Img>
+                                 <div><p className='desc_font'>{x.name}<br></br><p style = {{fontWeight:"500",color:"green"}} >{x.desc}</p></p>
+   
+                              </div>
                                    
 
-                                <p>{x.date?x.date.split("-").reverse().join("-"):<></>}</p>
-                                Going: {x.visiting.length>0 ? x.visiting.length:"0"}
+                                {x.date?x.date.split("-").reverse().join("-"):<></>}
+                                
                             </Card.Body>
-                               <Card.Footer className='desc_font'> <Button disabled = { x.visiting.includes( profile_id)? true:false }  onClick={e=>addVisiting(x._id,profile_id)} size = "sm" variant = "success"> Going </Button> </Card.Footer>
+                             <Card.Footer>  <Button style = {{color:"white",fontWeight:"bold"}} disabled = { x.visiting.includes( profile_id)? true:false }  onClick={e=>addVisiting(x._id,profile_id)} size = "sm" variant = "warning"> Going </Button> 
+                             <Button style = {{marginLeft:"5px",width:"80px"}} disabled = { x.visiting.includes( profile_id)? true:false }  onClick={e=>addVisiting(x._id,profile_id)} size = "sm" variant = "outline-success"> view more </Button> </Card.Footer>
                                     
                                   
                             </Card>
